d8b57c2c913ef38310f3c60114758336879f2da6
tusharhow <tusharsoftdev@gmail.com>
2022-05-18 23:08:31 -0700

    first commit

--- lib/platform_info.dart
+++ lib/platform_info.dart
@@ -1,3 +1,54 @@
-library platform_info;
+import 'package:flutter/foundation.dart';
 
-export 'src/platform_info.dart';
+/// Allows you to determine platform info such as operating system or environment
+class PlatformInfo {
+  static PlatformInfo singleton = PlatformInfo._internal();
+
+  factory PlatformInfo() {
+    return singleton;
+  }
+
+  PlatformInfo._internal();
+
+  /// This parameter checks if the current platform is macos, linux or windows. THE WEB PLATFORM IS NOT INCLUDED!
+  bool get isDesktop =>
+      defaultTargetPlatform == TargetPlatform.macOS ||
+      defaultTargetPlatform == TargetPlatform.linux ||
+      defaultTargetPlatform == TargetPlatform.windows;
+
+  /// This parameter checks if the current platform is iOS, Android or Fuchsia.
+  bool get isMobile =>
+      defaultTargetPlatform == TargetPlatform.iOS ||
+      defaultTargetPlatform == TargetPlatform.android ||
+      defaultTargetPlatform == TargetPlatform.fuchsia;
+
+  /// This parameter checks if the current platform is web or not.
+  bool get isWeb =>
+      defaultTargetPlatform != TargetPlatform.macOS &&
+      defaultTargetPlatform != TargetPlatform.linux &&
+      defaultTargetPlatform != TargetPlatform.windows &&
+      defaultTargetPlatform != TargetPlatform.iOS &&
+      defaultTargetPlatform != TargetPlatform.android &&
+      defaultTargetPlatform != TargetPlatform.fuchsia;
+
+  /// This parameter calls the isDesktop and isMobile methods to detect if the current platform is desktop or web
+  bool get isDesktopOrWeb => isDesktop || !isMobile;
+
+  /// Check if the platform on which the code is running is iOS
+  bool get isIOS => defaultTargetPlatform == TargetPlatform.iOS;
+
+  /// Check if the platform on which the code is running is Android
+  bool get isAndroid => defaultTargetPlatform == TargetPlatform.android;
+
+  /// Check if the platform on which the code is running is Fuchsia
+  bool get isFuchsia => defaultTargetPlatform == TargetPlatform.fuchsia;
+
+  /// Check if the platform on which the code is running is Windows
+  bool get isWindows => defaultTargetPlatform == TargetPlatform.windows;
+
+  /// Check if the platform on which the code is running is Linux
+  bool get isLinux => defaultTargetPlatform == TargetPlatform.linux;
+
+  /// Check if the platform on which the code is running is macOS
+  bool get isMacOS => defaultTargetPlatform == TargetPlatform.macOS;
+}
--- test/platform_info_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-
-import 'package:platform_info/platform_info.dart';
-
-void main() {
-  test('adds one to input values', () {
-   
-  });
-}
