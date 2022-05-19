725e8675d3531a27632558f4c7052a1845376810
tusharhow <tusharsoftdev@gmail.com>
2022-05-18 22:55:59 -0700

    first commit

---
+++ lib/platform_info.dart
@@ -0,0 +1,3 @@
+library platform_info;
+
+export 'src/platform_info.dart';
---
+++ test/platform_info_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:platform_info/platform_info.dart';
+
+void main() {
+  test('adds one to input values', () {
+   
+  });
+}
